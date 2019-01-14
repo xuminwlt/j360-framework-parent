7d2bd0363e3cdc7bf1f71abc17946d7452fa0200
min_xu <xumin_wlt@163.com>
2019-01-14 11:07:55 +0800

    rebuild web shiro

---
+++ j360-boot-starts-web/src/main/java/me/j360/framework/boot/shiro/JwtSignature.java
@@ -0,0 +1,152 @@
+package me.j360.framework.boot.shiro;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.vip.vjtools.vjkit.collection.ArrayUtil;
+import me.j360.framework.base.constant.DefaultErrorCode;
+import me.j360.framework.base.exception.ServiceException;
+import me.j360.framework.boot.shiro.dao.SessionStorageDAO;
+import me.j360.framework.common.exception.BizException;
+import me.j360.framework.common.util.JwtUtil;
+import me.j360.framework.common.web.context.BaseSessionUser;
+import me.j360.framework.common.web.context.DefaultSessionUser;
+import me.j360.framework.common.web.context.SessionContext;
+import me.j360.framework.common.web.context.SessionUser;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+/**
+ * @author: min_xu
+ * @date: 2019/1/11 5:11 PM
+ * 说明：
+ */
+public class JwtSignature {
+
+    private static String Authorization = "Authorization";
+    private static String CID = "cid";
+    private static String CODE = "code";
+    //Auth 定义jwt内容
+    public static final String JWT_AUD_GUEST = "guest";
+    public static final String JWT_AUD_USET = "user";
+    public static final String JWT_ISSUER = "J360";
+
+    private Algorithm algorithm;
+    private SessionStorageDAO sessionStorageDAO;
+
+    public JwtSignature(Algorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public JwtSignature(Algorithm algorithm, SessionStorageDAO sessionStorageDAO) {
+        this.sessionStorageDAO = sessionStorageDAO;
+        this.algorithm = algorithm;
+    }
+
+    public String createGuest(String cid) {
+        return createJwt(cid, null, JWT_AUD_GUEST);
+    }
+
+    /**
+     * pre step none
+     * @param request
+     * @return
+     */
+    public String createGuest(HttpServletRequest request) {
+        //storage
+        return createGuest(getCid(request));
+    }
+
+    public String createUser(String cid, String sessionId) {
+        return createJwt(cid, sessionId, JWT_AUD_USET);
+    }
+
+    /**
+     * pre step AuthcFilter
+     * @return
+     */
+    public String createUser() {
+        BaseSessionUser sessionUser = SessionContext.getBaseSessionUser();
+        //storage
+        String cid = sessionUser.getCid();
+        String sessionId = sessionUser.getSessionId();
+        String jwt = createUser(cid, sessionId);
+        return jwt;
+    }
+
+
+    private String createJwt(String cid, String uuid, String aud) {
+        try {
+            DateTime dateTime = new DateTime();
+            String token = JWT.create()
+                    .withJWTId(cid)
+                    .withIssuer(JWT_ISSUER)
+                    .withIssuedAt(dateTime.toDate())
+                    .withExpiresAt(dateTime.plusYears(1).toDate())
+                    .withAudience(aud)
+                    .withSubject(Optional.ofNullable(uuid).orElse(""))
+                    .withClaim("nonce", System.currentTimeMillis())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception){
+            //Invalid Signing configuration / Couldn't convert Claims.
+            throw new ServiceException(DefaultErrorCode.SYSTEM_ERROR,  exception);
+        }
+    }
+
+
+    public String getJwt(HttpServletRequest httpServletRequest) {
+        String authorization = httpServletRequest.getHeader(Authorization);
+        if (StringUtils.isEmpty(authorization)) {
+            throw BizException.bizException.clone(DefaultErrorCode.AUTH_ACCESS_SESSION_ERROR);
+        }
+        if(StringUtils.isEmpty(authorization) || ! authorization.startsWith("Bearer ")){
+            throw BizException.bizException.clone(DefaultErrorCode.AUTH_ACCESS_SESSION_ERROR);
+        }
+        return authorization.substring(7);
+    }
+
+    public JwtUtil.JwtOptions decode(String token) {
+        DecodedJWT jwt = JwtUtil.verify(algorithm, JWT_ISSUER, token);
+        return JwtUtil.JwtOptions.builder().subject(jwt.getSubject())
+                .signature(jwt.getSignature())
+                .audience(ArrayUtil.toArray(jwt.getAudience(), String.class))
+                .cid(jwt.getId())
+                .issueAt(jwt.getIssuedAt())
+                .issuer(jwt.getIssuer())
+                .nonce(jwt.getClaim("nonce").asLong())
+                .expiresAt(jwt.getExpiresAt())
+                .build();
+    }
+
+    public String signature(JwtUtil.JwtOptions options) {
+        String credentials = JwtUtil.sigurate(algorithm, options);
+        return credentials;
+    }
+
+    //get cid
+    public String getCid(HttpServletRequest request) {
+        return request.getParameter(CID);
+    }
+
+    //get code
+    public String getCode(HttpServletRequest request) {
+        return request.getParameter(CODE);
+    }
+
+    //create Session User
+    private SessionUser createSessionUser(String jwt, String cid, Long uid, String sessionId) {
+        DefaultSessionUser sessionUser = DefaultSessionUser.builder()
+                .jwt(jwt)
+                .build();
+
+        sessionUser.setCid(cid);
+        sessionUser.setUid(uid);
+        sessionUser.setSessionId(sessionId);
+        return sessionUser;
+    }
+}
---
+++ j360-boot-starts-web/src/main/java/me/j360/framework/boot/shiro/filter/UserLoginSessionFilter.java
@@ -0,0 +1,40 @@
+package me.j360.framework.boot.shiro.filter;
+
+import me.j360.framework.boot.shiro.JwtSignature;
+import me.j360.framework.common.web.context.SessionContext;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+import org.apache.shiro.web.filter.PathMatchingFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @author: min_xu
+ * @date: 2019/1/11 4:05 PM
+ * 说明：
+ */
+public class UserLoginSessionFilter extends PathMatchingFilter {
+
+    private JwtSignature jwtSignature;
+
+    public UserLoginSessionFilter(JwtSignature jwtSignature) {
+        this.jwtSignature = jwtSignature;
+    }
+
+    @Override
+    protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+        //设置该请求为无状态请求
+        request.setAttribute(DefaultSubjectContext.SESSION_CREATION_ENABLED, Boolean.FALSE);
+        String jwt = jwtSignature.createUser();
+        return true;
+    }
+
+
+    /**
+     *
+     */
+    @Override
+    public void afterCompletion(ServletRequest request, ServletResponse response, Exception exception) throws Exception {
+        SessionContext.clear();
+    }
+}
