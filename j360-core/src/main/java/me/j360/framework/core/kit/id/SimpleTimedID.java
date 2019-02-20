845e000a38b7eb7da42d6a1783284241143fa5c7
xumin <xumin.wlt@gmail.com>
2019-02-20 13:56:23 +0800

    add validator

---
+++ j360-core/src/main/java/me/j360/framework/core/kit/id/SimpleTimedID.java
@@ -0,0 +1,14 @@
+package me.j360.framework.core.kit.id;
+
+/**
+ * @author: min_xu
+ * 22位基于时间单位毫秒规整的ID生成规则
+ * 201901010101001 + (0-99) + (0-999)
+ * 有限使用情况
+ * 最多承载100个集群,单位毫秒1000个并发量
+ */
+public class SimpleTimedID {
+
+
+
+}
