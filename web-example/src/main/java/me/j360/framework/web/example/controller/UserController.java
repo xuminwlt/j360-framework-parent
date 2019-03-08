439e8ba77b08ae69510df263618d3a3d8202105b
xumin <xumin.wlt@gmail.com>
2019-03-08 15:40:02 +0800

    add shiro example

--- web-example/src/main/java/me/j360/framework/web/example/controller/UserController.java
+++ web-example/src/main/java/me/j360/framework/web/example/controller/UserController.java
@@ -16,11 +16,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class UserController extends BaseController {
 
 
+    //Add Http Header
+    //Authorization:Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ1c2VyIiwic3ViIjoidXVpZDIiLCJpc3MiOiJqMzYwLm1lIiwiZXhwIjoxNTgzNjQ2Nzk0LCJpYXQiOjE1NTIwMjQzOTQsIm5vbmNlIjoxNTUyMDI0Mzk0NjAwLCJqdGkiOiIxMTEifQ.bLJu7dss2BwkyOEtk7CKIxHKtkQzUOaRXLJySRnzX18
+    //Client-Agent:ISSUE/1.0.0/1/2/7.1/iPhone 5s (A1457/A1518/A1528/A1530)/7EAB70B1-624F-463A-943C-E7FF235A9A0C/wifi/iOS
+
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     @ResponseBody
     public String info() {
         log.info("login user: {}", SessionContext.getBaseSessionUser());
         log.info("login uid: {}", SessionContext.getSessionId());
+        log.info("login uid: {}", SessionContext.getBaseSessionUser().getAgent());
         return "SUCCESS";
     }
 
